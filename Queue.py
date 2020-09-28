81279606dba01e01baafb130b26b1c3b754af587
Ars187 <71893695+Ars187@users.noreply.github.com>
2020-09-28 21:24:40 +0530

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -1,25 +1,26 @@
 #Queue
-def insert(q,item):
-    q.append(item)
-def delete(q):
-    if len(q)==0:
-        return(-1)
-    else:
-        return(q.pop(0))    
-q=[]
+class queue:
+    def __init__(self):
+        self.items=[]
+    def is_empty(self):
+        return self.items==[]
+    def insert(self,value):
+        self.items.append(value)
+    def delete(self):
+        return self.items.pop(0)   
+q=Queue()
 while True:
     print("\n1 Insert\n2 Delete\n3 Exit\n")
     c=int(input("Enter your choice : "))
     if c==1:
-        item=int(input("Enter the item : "))
-        insert(q,item)
-        print(q)
+        value=int(input("Enter the item : "))
+        q.insert(value)
+        print(q.items)
     elif c==2:
-        x=delete(q)
-        if x==-1:
+        if q.is_empty():
             print("Underflow")
         else:
-            print("\nPopped item : ",x)
+            print("\nPopped item : ",q.delete())
     elif c==3:
         exit()
     else:
