bb59f44a5b9067fcd2fa1627e3846f63d0fb4adf
Ars187 <71893695+Ars187@users.noreply.github.com>
2020-09-28 21:16:03 +0530

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,25 +1,30 @@
 #Stack
-def push(stack,item):
-    stack.append(item)
-def pops(s):
-    if len(stack)==0:
-        return(-1)
-    else:
-        return(stack.pop())    
-stack=[]
+class Stack:
+    def __init__(self):
+        self.items = []
+ 
+    def is_empty(self):
+        return self.items == []
+ 
+    def push(self, data):
+        self.items.append(data)
+ 
+    def pop(self):
+        return self.items.pop()
+    
+s=Stack()
 while True:
     print("\n1 Push\n2 Pop\n3 Exit\n")
     c=int(input("Enter your choice : "))
     if c==1:
-        item=int(input("Enter the item : "))
-        push(stack,item)
-        print(stack)
+        data=int(input("Enter the item : "))
+        s.push(data)
+        print(s.items)
     elif c==2:
-        x=pops(stack)
-        if x==-1:
+        if s.is_empty():
             print("Underflow")
         else:
-            print("\nPopped item : ",x)
+            print("\nPopped item : ",s.pop())
     elif c==3:
         exit()
     else:
